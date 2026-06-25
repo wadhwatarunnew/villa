19fc6e69c98130e21720d7c132c6290230a37a07
Rishav Saini <rishavsaini1700@gmail.com>
2026-06-25 08:59:41 +0530

    worked on home page layout and update component imports

--- src/villa/features/home/pages/home-page/home-page.component.ts
+++ src/villa/features/home/pages/home-page/home-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeHighlightComponent } from '../../components/home-highlight/home-highlight.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'villa-home-page',
   standalone: true,
-  imports: [CommonModule, HomeHighlightComponent],
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  imports: [CommonModule, RouterModule],
+  templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {}

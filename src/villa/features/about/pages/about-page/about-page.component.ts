362343c07e4c8fcfa46e9388b1ba9cd69051bc8d
tarun wadhwa <wadhwatraun2026@gmail.com>
2026-06-27 20:31:17 +0530

    fic changes related to home page

--- src/villa/features/about/pages/about-page/about-page.component.ts
+++ src/villa/features/about/pages/about-page/about-page.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {
   AboutHeroComponent,
   AboutIntroComponent,
-  AboutStatsBannerComponent,
+  AboutStatsBannerComponent,    
   AboutValuesComponent,
   AboutMissionVisionComponent,
   AboutContactComponent,

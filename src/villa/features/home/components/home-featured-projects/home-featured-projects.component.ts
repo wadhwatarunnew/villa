1c0d501199d0a7e1af8bc119c454167afe1e1c48
tarun wadhwa <wadhwatraun2026@gmail.com>
2026-06-30 21:48:06 +0530

    images updated on home page

--- src/villa/features/home/components/home-featured-projects/home-featured-projects.component.ts
+++ src/villa/features/home/components/home-featured-projects/home-featured-projects.component.ts
@@ -9,14 +9,14 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeFeaturedProjectsComponent {
   projects = [
-    { title: 'The Oberoi, Rajasthan', location: 'Ranthambore', image: 'assets/images/villatent.webp' },
-    { title: 'Al Maha Resort', location: 'Dubai, UAE', image: 'assets/images/villatent.webp' },
-    { title: 'Kanha Earth Lodge', location: 'Madhya Pradesh, India', image: 'assets/images/villatent.webp' },
-    { title: 'Four Seasons Resort', location: 'Bali, Indonesia', image: 'assets/images/villatent.webp' },
-    { title: 'Sonoran Resorts', location: 'Saudi Arabia', image: 'assets/images/villatent.webp' },
-    { title: 'Aman-i-Khas', location: 'Ranthambore, India', image: 'assets/images/villatent.webp' },
-    { title: 'Shinta Mani Wild', location: 'Cardamom Mountains, Cambodia', image: 'assets/images/villatent.webp' },
-    { title: 'Banyan Tree AlUla', location: 'AlUla, Saudi Arabia', image: 'assets/images/villatent.webp' },
+    { title: 'The Oberoi, Rajasthan', location: 'Ranthambore', image: 'assets/images/villatent4.webp' },
+    { title: 'Al Maha Resort', location: 'Dubai, UAE', image: 'assets/images/villatent5.webp' },
+    { title: 'Kanha Earth Lodge', location: 'Madhya Pradesh, India', image: 'assets/images/villatent6.webp' },
+    { title: 'Four Seasons Resort', location: 'Bali, Indonesia', image: 'assets/images/villatent7.webp' },
+    { title: 'Sonoran Resorts', location: 'Saudi Arabia', image: 'assets/images/villatent1.webp' },
+    { title: 'Aman-i-Khas', location: 'Ranthambore, India', image: 'assets/images/villatent3.webp' },
+    { title: 'Shinta Mani Wild', location: 'Cardamom Mountains, Cambodia', image: 'assets/images/villatent8.webp' },
+    { title: 'Banyan Tree AlUla', location: 'AlUla, Saudi Arabia', image: 'assets/images/villatent9.webp' },
   ];
 
   scrollSlider(container: HTMLElement, direction: 'left' | 'right') {
--- src/villa/features/home/components/home-tent-collection/home-tent-collection.component.ts
+++ src/villa/features/home/components/home-tent-collection/home-tent-collection.component.ts
@@ -12,34 +12,34 @@ export class HomeTentCollectionComponent {
     {
       index: '01',
       title: 'Safari Tents',
-      image: 'assets/images/villatent.webp',
+      image: 'assets/images/villatent6.webp',
       description: 'Timeless designs with<br />ultimate comfort',
     },
     {
       index: '02',
       title: 'Bell Tents',
-      image: 'assets/images/villatent.webp', description: 'Classic elegance for<br />intimate getaways',
+      image: 'assets/images/villatent7.webp', description: 'Classic elegance for<br />intimate getaways',
     },
     {
       index: '03',
       title: 'Resort Tents',
-      image: 'assets/images/villatent.webp', description: 'Perfect for resorts<br />& retreats',
+      image: 'assets/images/villatent8.webp', description: 'Perfect for resorts<br />& retreats',
     },
     {
       index: '04',
       title: 'Geodesic Domes',
-      image: 'assets/images/villatent.webp', description: 'Modern, strong &<br />perfect for all climates',
+      image: 'assets/images/villatent9.webp', description: 'Modern, strong &<br />perfect for all climates',
     },
-    {
-      index: '05',
-      title: 'Luxury Villas',
-      image: 'assets/images/villatent.webp', description: 'Spacious luxury for<br />exclusive stays',
-    },
-    {
-      index: '06',
-      title: 'Custom Structures',
-      image: 'assets/images/villatent.webp', description: 'Fully customized tents<br />tailored to your vision',
-    },
+    // {
+    //   index: '05',
+    //   title: 'Luxury Villas',
+    //   image: 'assets/images/villatent5.webp', description: 'Spacious luxury for<br />exclusive stays',
+    // },
+    // {
+    //   index: '06',
+    //   title: 'Custom Structures',
+    //   image: 'assets/images/villatent6.webp', description: 'Fully customized tents<br />tailored to your vision',
+    // },
   ];
 }
 

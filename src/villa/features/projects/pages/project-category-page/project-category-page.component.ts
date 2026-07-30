55a66a02ef0f33cf6c7286a19a746e32d5cac652
Rishav Saini <rishavsaini1700@gmail.com>
2026-07-30 23:12:24 +0530

    fixed changes

--- src/villa/features/projects/pages/project-category-page/project-category-page.component.ts
+++ src/villa/features/projects/pages/project-category-page/project-category-page.component.ts
@@ -6,8 +6,29 @@ import { CommonCtaComponent } from '../../../../shared/components/common-cta/com
 
 @Component({ selector: 'villa-project-category-page', standalone: true, imports: [CommonModule, RouterModule, CommonCtaComponent], templateUrl: './project-category-page.component.html' })
 export class ProjectCategoryPageComponent {
+  readonly pageSize = 8;
   category = projectCategories[0];
   readonly images = projectCategories;
-  constructor(route: ActivatedRoute) { route.paramMap.subscribe(params => this.category = projectCategories.find(item => item.slug === params.get('category')) ?? projectCategories[0]); }
+  visibleCount = this.pageSize;
+
+  constructor(route: ActivatedRoute) {
+    route.paramMap.subscribe(params => {
+      this.category = projectCategories.find(item => item.slug === params.get('category')) ?? projectCategories[0];
+      this.visibleCount = this.pageSize;
+    });
+  }
+
+  get displayedProjects(): string[] {
+    return this.category.projects.slice(0, this.visibleCount);
+  }
+
+  get hasMoreProjects(): boolean {
+    return this.visibleCount < this.category.projects.length;
+  }
+
+  loadMore(): void {
+    this.visibleCount += this.pageSize;
+  }
+
   routeSlug(project: string): string { return projectRouteSlug(project); }
 }

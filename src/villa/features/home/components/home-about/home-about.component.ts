256f04d3f63e03ae37dd56dc1abeaf27009b6d5b
Rohit Kumar <rohit84086@gmail.com>
2026-08-31 04:09:53 -0500

    new fixes

--- src/villa/features/home/components/home-about/home-about.component.ts
+++ src/villa/features/home/components/home-about/home-about.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'villa-home-about',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home-about.component.html',
   styleUrls: ['./home-about.component.scss']
 })
 export class HomeAboutComponent {
   @Input() aboutSection: any = {};
+  @Input() features: any = {};
 }
